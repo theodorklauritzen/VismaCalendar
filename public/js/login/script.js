20651b62a8ba5ab32ea88882a86bde0a903b24dc
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-10-02 12:47:58 +0200

    Changed Valler to the default school

--- public/js/login/script.js
+++ public/js/login/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   const schoolInput = document.getElementById('school');
-  schoolInput.value = localStorage.getItem('school') ? localStorage.getItem('school') : schoolInput.value
+  schoolInput.value = localStorage.getItem('school') ? localStorage.getItem('school') : "valler"
   schoolInput.addEventListener('input', e => {
     localStorage.setItem('school', e.target.value);
   });
