3ae1639a82542a5747a0b82131f01b65793be939
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-10-17 11:09:58 +0200

    Added a alert message befor login

--- public/js/login/script.js
+++ public/js/login/script.js
@@ -1,4 +1,16 @@
+function agree() {
+  localStorage.setItem('agree', true);
+
+  document.getElementById("security-alert").innerHTML = ""
+  document.getElementById("loginBtn").disabled = false
+}
+
 window.onload = function() {
+  if (localStorage.getItem('agree') != "true") {
+    document.getElementById("loginBtn").disabled = true
+    document.getElementById("security-alert").innerHTML = '<div class="alert alert-danger alert-security"><strong>ADVARSEL:</strong> Før du logger inn må du være klar over <a class="alert-link" href="/about#risk">sikkerhetsrisikoen</a> og du må godta <a class="alert-link" href="/terms">betingelsene</a>.<br><button class="btn btn-security-alert" type="button" onclick="agree()">Jeg forstår</button></div>'
+  }
+
   const schoolInput = document.getElementById('school');
   schoolInput.value = localStorage.getItem('school') ? localStorage.getItem('school') : "valler"
   schoolInput.addEventListener('input', e => {
