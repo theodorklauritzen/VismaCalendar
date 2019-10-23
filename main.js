e5497f8250af112dbe9063af1fc9da9b5f54c9a3
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2019-10-23 14:47:49 +0200

    Updated the status that the redirect sends

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-  console.log(req)
-  res.redirect(redirectDomain + req.originalUrl)
+  res.redirect(308, redirectDomain + req.originalUrl)
 })
 
 app.listen(process.env.PORT || 80, () => console.log(`Listening on: ${process.env.PORT || 80}`))
