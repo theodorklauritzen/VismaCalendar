4b0d909e61c011152edac7519c8e8f5b389f3cef
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2021-05-11 20:41:19 +0200

    Added a --no-sandbox argument to make puppeteer to work on Heroku

--- fetchData.js
+++ fetchData.js
@@ -2,7 +2,8 @@ const puppeteer = require('puppeteer');
 
 async function getTimetable(login_name, password, schoolLink, date, organization, callback) {
   const browser = await puppeteer.launch({
-    headless: true // Set to false to watch the bot
+    headless: true, // Set to false to watch the bot
+    args: ['--no-sandbox'] // Need this to work on Heroku
   });
   const page = await browser.newPage();
 
